2045b313ea9245c582744350f87e4e46014e2827
Christoph <christoph.roessing@gmx.de>
2022-01-03 09:50:11 +0000

    still looking for memory leak

--- src/VideoServer.cpp
+++ src/VideoServer.cpp
@@ -2,7 +2,6 @@
 
 #include <iostream>
 
-
 void VideoServerClass::StartVideoServerThread()
 {
   _VideoServerThread = thread(&VideoServerClass::SessionRunLoop, this);
@@ -12,19 +11,21 @@ void VideoServerClass::StopVideoServerThread()
   //create empty frame and set it to queue to trigger thread exit
   unique_ptr<Mat> frame = make_unique<Mat>(Mat(Size(0, 0), CV_8U));
 
-  input_queue.send(move(frame));
+  input_queue.sendAndClear(move(frame));
   cout << "Waiting for Detector thread to join.\n";
   _VideoServerThread.join();
 }
 
 void VideoServerClass::SessionRunLoop()
 {
-  
- cout << "VideoServer thread started.\n";
+
+  cout << "VideoServer thread started.\n";
   while (true)
   {
     //wait for empty detection to be received
-    unique_ptr<Mat> frame = input_queue.receive();
+    unique_ptr<Mat> frame(input_queue.receive());
+
+    //cout << "Sender Queue Size: " << input_queue.GetSize() << "\n";
 
     //cout << "Try to write frame to " << _dest_IP << " with size: " << frame->size[0] << "...";
     //empty frame --> cleanup and exit thread
@@ -36,9 +37,9 @@ void VideoServerClass::SessionRunLoop()
       cout << "Try to open Frame Writer\n";
       //x264enc tune=zerolatency speed-preset=ultrafast
       //video.open("appsrc ! videoconvert ! x264enc me=hex tune=zerolatency speed-preset=ultrafast key-int-max=50 intra-refresh=true ! h264parse ! rtph264pay config-interval=5 pt=96 ! udpsink host=" + dest_IP+ " port=5000 sync=false",0, 20, frame.size(), true);
-       int fourcc =    VideoWriter::fourcc('H','2','6','4');
-      _video.open("appsrc ! videoconvert ! x264enc tune=zerolatency speed-preset=ultrafast key-int-max=2 ! h264parse ! rtph264pay config-interval=5 pt=96 ! udpsink host=" + _dest_IP + " port=5000 sync=false",CAP_GSTREAMER,fourcc, 5, frame->size(), true);
-       
+      int fourcc = VideoWriter::fourcc('H', '2', '6', '4');
+      _video.open("appsrc ! videoconvert ! x264enc tune=zerolatency speed-preset=ultrafast key-int-max=2 ! h264parse ! rtph264pay config-interval=5 pt=96 ! udpsink host=" + _dest_IP + " port=5000 sync=false", CAP_GSTREAMER, fourcc, 5, frame->size(), true);
+
       if (!_video.isOpened())
       {
         cout << "Frame Writer not availbale!!!!!!\n";
@@ -48,7 +49,6 @@ void VideoServerClass::SessionRunLoop()
     }
 
     _video.write(*frame);
-
-     cout << "written.\n";
+    //cout << "Video written";
   }
 }
